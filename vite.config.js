bf2e66cce70ad9499e42c5502acab50172e82b55
JonasIngebretsen <tehjonez@gmail.com>
2022-03-05 19:23:12 +0100

    Replaced PrePros with Vite

---
+++ vite.config.js
@@ -0,0 +1,69 @@
+import autoprefixer from 'autoprefixer';
+import combineSelectors from 'postcss-combine-duplicated-selectors';
+import combineMediaQueries from 'postcss-combine-media-query';
+import compress from 'vite-plugin-compression';
+import imageMin from 'vite-plugin-imagemin';
+
+const path = require('path')
+const isProd = process.env.NODE_ENV === 'production';
+
+export default {
+	css: {
+		postcss: {
+			plugins: [
+				combineMediaQueries(),
+				combineSelectors({ removeDuplicatedValues: true }),
+				autoprefixer(),
+			],
+		},
+	},
+	build: {
+
+		minify: isProd,
+
+		root: path.join(__dirname, "src"),
+		outDir: path.join(__dirname, "dist"),
+
+		rollupOptions: {
+			input: {
+				'fields': path.resolve(__dirname, 'src', 'js', 'Fields.js'),
+				'style': path.resolve(__dirname, 'src', 'scss', 'style.scss'),
+				'demo': path.resolve(__dirname, 'src', 'scss', 'demo.scss'),
+			},
+			output: {
+				entryFileNames: `[name].dist.js`,
+				chunkFileNames: `[name].dist.js`,
+				assetFileNames: `[name].dist.[ext]`
+			}
+		},
+
+	},
+	plugins: [
+		imageMin({
+			svgo: {
+				plugins: [
+				{ name: 'RemoveTitle', active: false },
+				{ name: 'RemoveDescription', active: false },
+				{ name: 'RemoveViewBox', active: false },
+				{ name: 'removeDimensions', active: true },
+				{ name: 'removeScriptElement', active: true },
+				{ name: 'removeStyleElement', active: true },
+				],
+			},
+		}),
+		compress({
+			algorithm: 'brotliCompress',
+		}),
+	],
+
+	server: {
+		open: true,
+		// proxy: {
+		// 	"/base": {
+		// 		target: "http://localhost:19000",
+		// 		changeOrigin: true,
+		// 		rewrite: (path) => path.replace(/^\/base/, ""),
+		// 	},
+		// },
+	},
+};
